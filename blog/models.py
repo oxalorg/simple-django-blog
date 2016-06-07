74a56184ad015f588e4f70b678d0323f6ea11eb6
Mitesh Shah <miteshninja@gmail.com>
2016-06-07 21:48:05 +0530

    Added 'post' to blog models.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    post_title = models.CharField(max_length=250)
+    post_body = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    view_count = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
