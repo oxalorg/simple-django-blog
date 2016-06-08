f8e02cc316ba72cf8b6bee7c6873ceeb096522b3
Mitesh Shah <miteshninja@gmail.com>
2016-06-08 18:13:56 +0530

    Customized admin index/interface of Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Post
+
+
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['post_title', 'post_body']}),
+        ('Meta Info', {'fields': ['pub_date']})
+    ]
+    list_display = ('post_title', 'pub_date')
+    search_fields = ['post_title']
+
+
+admin.site.register(Post, PostAdmin)
