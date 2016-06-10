1948ef6eb86fdea6f84d764c22082f20af3c02b3
Mitesh Shah <miteshninja@gmail.com>
2016-06-10 22:21:34 +0530

    Random changes.

---
+++ conf/renew-letsencrypt.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd /opt/letsencrypt/ ./letsencrypt-auto --config /etc/letsencrypt/configs/earthlyhumans.com.conf certonly
+
+if [ $? -ne 0 ]
+ then
+        ERRORLOG=`tail /var/log/letsencrypt/letsencrypt.log`
+        echo -e "The Let's Encrypt cert has not been renewed! \n \n" \
+                 $ERRORLOG
+ else
+        nginx -s reload
+fi
+
+exit 0
